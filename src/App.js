05d79f36e69cef10b43fb60bad488166fb73fd87
Edouard de Laage <edouarddelaage@gmail.com>
2018-09-27 16:51:31 +0200

    Add Axios on Reddit and fix webPreferences an webSecurity in electron.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios'
+
 class App extends Component {
+
+  componentDidMount() {
+    axios.get('http://reddit.com/r/aww.json')
+    .then(response => {
+      console.log(response)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Cras justo odio</li>
-          <li class="list-group-item">Dapibus ac facilisis in</li>
-          <li class="list-group-item">Morbi leo risus</li>
-          <li class="list-group-item">Porta ac consectetur ac</li>
-          <li class="list-group-item">Vestibulum at eros</li>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Cras justo odio</li>
+          <li className="list-group-item">Dapibus ac facilisis in</li>
+          <li className="list-group-item">Morbi leo risus</li>
+          <li className="list-group-item">Porta ac consectetur ac</li>
+          <li className="list-group-item">Vestibulum at eros</li>
         </ul>
       </div>
     );
